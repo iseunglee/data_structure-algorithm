cc9fb8f260174caa0d4f5abb0cb4d90b51e74e47
iseunglee <iseunglee99@gmail.com>
2024-05-27 09:02:55 +0900

    5월27일

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -3,3 +3,21 @@ class BTNode:
         self.data = elem
         self.left = left
         self.right = right
+
+    def preorder(n): # 이진트리의 전위순회
+        if n is not None:
+            print(n.data, end=' ')
+            preorder(n.left)
+            preorder(n.right)
+    
+    def inorder(n): # 이진트리의 중위순회
+        if n is not None:
+            inorder(n.left)
+            print(n.data, end=' ')
+            inorder(n.right)
+
+    def postorder(n):
+        if n is not None:
+            postorder(n.left)
+            postorder(n.right)
+            print(n.data, end=' ')
