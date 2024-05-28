87963731661a09d463591dfbbeb6d992937f1a39
iseunglee <iseunglee99@gmail.com>
2024-05-28 21:52:22 +0900

    5월28일

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -1,23 +1,71 @@
+from circular_queue import ArrayQueue
+
 class BTNode:
     def __init__(self, elem, left=None, right=None):
         self.data = elem
         self.left = left
         self.right = right
 
-    def preorder(n): # 이진트리의 전위순회
-        if n is not None:
-            print(n.data, end=' ')
-            preorder(n.left)
-            preorder(n.right)
-    
-    def inorder(n): # 이진트리의 중위순회
-        if n is not None:
-            inorder(n.left)
-            print(n.data, end=' ')
-            inorder(n.right)
+def preorder(n): # 이진트리의 전위순회
+    if n is not None:
+        print(n.data, end=' ')
+        preorder(n.left)
+        preorder(n.right)
 
-    def postorder(n):
-        if n is not None:
-            postorder(n.left)
-            postorder(n.right)
-            print(n.data, end=' ')
+def inorder(n): # 이진트리의 중위순회
+    if n is not None:
+        inorder(n.left)
+        print(n.data, end=' ')
+        inorder(n.right)
+
+def postorder(n): # 이진트리의 후위순회
+    if n is not None:
+        postorder(n.left)
+        postorder(n.right)
+        print(n.data, end=' ')
+
+def levelorder(root): # 이진 트리의 레벨 순회
+    queue = ArrayQueue() # 큐 객체 초기화
+    queue.enqueue(root) # 루트 노드 큐에 삽입
+    while not queue.isEmpty(): # 큐가 공백 상태가 아닌 동안,
+        n = queue.dequeue() # 큐에서 하나의 노드를 꺼내고
+        if n is not None: # 이 노드가 None이 아니면
+            print(n.data, end=' ') # n의 데이터를 프린트
+            queue.enqueue(n.left) # 큐에 왼쪽 자식을 삽입
+            queue.enqueue(n.right) # 큐에 오른쪽 자식을 삽입
+
+def count_node(n): # 이진트리의 노드 개수 구하기
+    if n is None:
+        return 0
+    else:
+        return count_node(n.left) + count_node(n.right) + 1
+    
+def calc_height(n): # 이진트리의 높이 구하기
+    if n is None:
+        return 0
+    
+    hLeft = calc_height(n.left)
+    hRight = calc_height(n.right)
+
+    if hLeft > hRight:
+        return hLeft + 1
+    else:
+        return hRight + 1
+
+# 코드 4.8: 이진트리 연산들의 테스트 프로그램
+if __name__ == "__main__":
+    d = BTNode('D', None, None)
+    e = BTNode('E', None, None)
+    b = BTNode('B', d, e)
+    f = BTNode('F', None, None)
+    c = BTNode('C', f, None)
+    root = BTNode('A', b, c)
+
+    print('\n   In-Order : ', end=''); inorder(root)
+    print('\n  Pre-Order : ', end=''); preorder(root)
+    print('\n Post-Order : ', end=''); postorder(root)
+    print('\nLevel-Order : ', end=''); levelorder(root)
+    print()
+
+    print(" 노드의 개수 = %d개" % count_node(root))
+    print(" 트리의 높이 = %d" % calc_height(root))
---
+++ Tree/circular_queue.py
@@ -0,0 +1,78 @@
+class ArrayQueue:
+    def __init__(self, capacity = 10): # 생성자 정의
+        self.capacity = capacity # 용량(고정)
+        self.array = [None] * capacity # 요소들을 저장할 배열
+        self.front = 0 # 전단 인덱스
+        self.rear = 0 # 후단 인덱스
+
+    def isEmpty(self): # 공백 상태
+        return self.front == self.rear
+    
+    def isFull(self): # 포화 상태
+        return self.front == (self.rear + 1) % self.capacity
+    
+    def enqueue(self, item): # 삽입 연산
+        if not self.isFull():
+            self.rear = (self.rear + 1) % self.capacity
+            self.array[self.rear] = item
+        else: pass # 오버플로우 에러
+    
+    def dequeue(self): # 삭제 연산
+        if not self.isEmpty():
+            self.front = (self.front + 1) % self.capacity
+            return self.array[self.front]
+        else: pass # 언더플로우 에러
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.array[(self.front + 1) % self.capacity]
+        else: pass
+    
+    def size(self):
+        return (self.rear - self.front + self.capacity) % self.capacity
+    
+    def display(self, msg): # 전체 요소를 출력, msg : 큐의 이름이나 메시지를 출력하기 위한 매개변수
+        print(msg, end = "= [")
+        for i in range(self.front + 1, self.front + 1 + self.size()):
+            print(self.array[i % self.capacity], end = " ")
+        print("]")
+    
+    # 링 버퍼 구현을 위한 수정된 enqueue 연산
+    def enqueue2(self, item):
+        self.rear = (self.rear + 1) % self.capacity
+        self.array[self.rear] = item # 일단 무조건 삽입
+        if self.isEmpty(): # front == rear
+            self.front = (self.front + 1) % self.capacity # 삽입 후 front 회전 -> 가장 오래된 요소 삭제
+    
+# 원형 큐 테스트
+if __name__ == "__main__":
+    import random
+    q = ArrayQueue(8)
+
+    q.display("초기상태")
+    while not q.isFull():
+        q.enqueue(random.randint(0, 100))
+    q.display("포화 상태")
+
+    print("삭제 순서: ", end = "")
+    while not q.isEmpty():
+        print(q.dequeue(), end = " ")
+    print()
+
+# 링 버퍼 테스트
+if __name__ == "__main__":
+    q = ArrayQueue(8)
+
+    q.display("초기상태")
+    for i in range(6):
+        q.enqueue2(i)
+    q.display("삽입 0-5")
+
+    q.enqueue2(6); q.enqueue2(7)
+    q.display("삽입 6, 7")
+
+    q.enqueue2(8); q.enqueue2(9)
+    q.display("삽입 8, 9")
+
+    q.dequeue(); q.dequeue()
+    q.display("삭제 x2")
